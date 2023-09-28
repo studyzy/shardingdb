0e28e2717da09c67902d3388fd9e9929bf0bcd98
DevinZeng <studyzy@qq.com>
2023-09-28 18:26:38 +0800

    refactor: Performance Test
    --story=1

--- leveldb_interface.go
+++ leveldb_interface.go
@@ -18,24 +18,12 @@ package shardingdb
 
 import (
 	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/iterator"
-	"github.com/syndtr/goleveldb/leveldb/opt"
-	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
 type LevelDbHandle interface {
-	Get(key []byte, ro *opt.ReadOptions) (value []byte, err error)
-	Has(key []byte, ro *opt.ReadOptions) (ret bool, err error)
-	NewIterator(slice *util.Range, ro *opt.ReadOptions) iterator.Iterator
+	CommonDbHandle
 	GetSnapshot() (*leveldb.Snapshot, error)
-	GetProperty(name string) (value string, err error)
-	Stats(s *leveldb.DBStats) error
-	SizeOf(ranges []util.Range) (leveldb.Sizes, error)
-	Close() error
 	OpenTransaction() (*leveldb.Transaction, error)
-	Write(batch *leveldb.Batch, wo *opt.WriteOptions) error
-	Put(key, value []byte, wo *opt.WriteOptions) error
-	Delete(key []byte, wo *opt.WriteOptions) error
-	CompactRange(r util.Range) error
-	SetReadOnly() error
 }
+
+var _ LevelDbHandle = (*leveldb.DB)(nil)
--- performance_test.go
+++ performance_test.go
@@ -89,16 +89,66 @@ func BenchmarkLeveldb_Get(b *testing.B) {
 }
 
 func TestShardingDbPerformance(t *testing.T) {
-
 	pathList := []string{"/data/leveldb", "/data1/leveldb", getTempDir()}
 	//remove all folder
 	for _, path := range pathList {
 		os.RemoveAll(path)
 	}
-
+	//Test shardingdb performance
 	db, _ := OpenFile(pathList, nil)
-	fmt.Printf("start shardingdb performance test,batch[%d] thread[%d] loop[%d],sharding[%d]，record count:%d\n",
-		batchSize, thread, loop, db.length, batchSize*thread*loop)
+	testDbPerformance(t, db, "shardingdb")
+	db.Close()
+
+	//Print every folder size
+	for _, path := range pathList {
+		size, _ := folderSize(path)
+		fmt.Printf("Folder[%s] size:%d\n", path, size)
+	}
+}
+
+func TestShardingDbReplicationPerformance(t *testing.T) {
+	pathList := []string{"/data/leveldb", "/data/leveldb1", "/data1/leveldb", "/data1/leveldb1", getTempDir(), getTempDir()}
+	var err error
+	dbs := make([]LevelDbHandle, len(pathList))
+	for i := 0; i < len(pathList); i++ {
+		//remove all folder
+		os.RemoveAll(pathList[i])
+		dbs[i], err = leveldb.OpenFile(pathList[i], nil)
+		if err != nil {
+			//close all opened db
+			for j := 0; j < i; j++ {
+				dbs[j].Close()
+			}
+			return
+		}
+	}
+	//Test shardingdb performance
+	db, _ := NewShardingDb(WithDbHandles(dbs...), WithReplication(2))
+	testDbPerformance(t, db, "Replication2ShardingDb")
+	db.Close()
+
+	//Print every folder size
+	for _, path := range pathList {
+		size, _ := folderSize(path)
+		fmt.Printf("Folder[%s] size:%d\n", path, size)
+	}
+}
+
+func TestLeveldbPerformance(t *testing.T) {
+	dir := getTempDir()
+	fmt.Printf("start leveldb performance test,batch[%d] thread[%d] loop[%d],Path[%s]，record count:%d\n",
+		batchSize, thread, loop, dir, batchSize*thread*loop)
+	db, _ := leveldb.OpenFile(dir, nil)
+	testDbPerformance(t, db, "leveldb")
+	db.Close()
+	//Print  folder size
+	size, _ := folderSize(dir)
+	fmt.Printf("Folder[%s] size:%d\n", dir, size)
+}
+
+func testDbPerformance(t *testing.T, db CommonDbHandle, testName string) {
+	fmt.Printf("start shardingdb performance test,batch[%d] thread[%d] loop[%d],record count:%d\n",
+		batchSize, thread, loop, batchSize*thread*loop)
 	wg := sync.WaitGroup{}
 	wg.Add(thread)
 	start := time.Now()
@@ -117,7 +167,8 @@ func TestShardingDbPerformance(t *testing.T) {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Printf("ShardingDb write batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
+	fmt.Printf("%s write batch[%d] thread[%d] loop[%d] cost:%v\n",
+		testName, batchSize, thread, loop, time.Now().Sub(start))
 	//Test Get performance
 	wg = sync.WaitGroup{}
 	wg.Add(thread)
@@ -134,7 +185,8 @@ func TestShardingDbPerformance(t *testing.T) {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Printf("ShardingDb get batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
+	fmt.Printf("%s get batch[%d] thread[%d] loop[%d] cost:%v\n",
+		testName, batchSize, thread, loop, time.Now().Sub(start))
 	//Test Get not found performance
 	wg = sync.WaitGroup{}
 	wg.Add(thread)
@@ -151,7 +203,8 @@ func TestShardingDbPerformance(t *testing.T) {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Printf("ShardingDb get not found batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
+	fmt.Printf("%s get not found batch[%d] thread[%d] loop[%d] cost:%v\n",
+		testName, batchSize, thread, loop, time.Now().Sub(start))
 	//Test delete performance
 	wg = sync.WaitGroup{}
 	wg.Add(thread)
@@ -170,7 +223,8 @@ func TestShardingDbPerformance(t *testing.T) {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Printf("ShardingDb delete batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
+	fmt.Printf("%s delete batch[%d] thread[%d] loop[%d] cost:%v\n",
+		testName, batchSize, thread, loop, time.Now().Sub(start))
 
 	//Test Iterator performance
 	wg = sync.WaitGroup{}
@@ -187,121 +241,16 @@ func TestShardingDbPerformance(t *testing.T) {
 					count++
 				}
 				iter.Release()
-				assert.Equal(t, batchSize/2, count)
+				assert.Equal(t, batchSize/2, count) //half of the data is deleted
 			}
 		}(i)
 	}
 	wg.Wait()
-	fmt.Printf("ShardingDb iterator batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
-	db.Close()
-	//Print every folder size
-	for _, path := range pathList {
-		size, _ := folderSize(path)
-		fmt.Printf("Folder[%s] size:%d\n", path, size)
-	}
+	fmt.Printf("%s iterator batch[%d] thread[%d] loop[%d] cost:%v\n",
+		testName, batchSize, thread, loop, time.Now().Sub(start))
+
 }
-func TestLeveldbPerformance(t *testing.T) {
-	dir := getTempDir()
-	fmt.Printf("start leveldb performance test,batch[%d] thread[%d] loop[%d],Path[%s]，record count:%d\n",
-		batchSize, thread, loop, dir, batchSize*thread*loop)
-	db, _ := leveldb.OpenFile(dir, nil)
-	wg := sync.WaitGroup{}
-	wg.Add(thread)
-	start := time.Now()
-	for i := 0; i < thread; i++ {
-		go func(thr int) {
-			defer wg.Done()
-			for j := 0; j < loop; j++ {
-				batch := new(leveldb.Batch)
-				for k := 0; k < batchSize; k++ {
-					value := stringToBytesWithPadding(fmt.Sprintf("value-%d", j*k), valueLength)
-					batch.Put([]byte(fmt.Sprintf("key-%02d-%03d-%03d", thr, j, k)), value)
-				}
-				err := db.Write(batch, nil)
-				assert.NoError(t, err)
-			}
-		}(i)
-	}
-	wg.Wait()
-	fmt.Printf("Leveldb write batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
-	//Test Get performance
-	wg = sync.WaitGroup{}
-	wg.Add(thread)
-	start = time.Now()
-	for i := 0; i < thread; i++ {
-		go func(thr int) {
-			defer wg.Done()
-			for j := 0; j < loop; j++ {
-				for k := 0; k < batchSize; k++ {
-					_, err := db.Get([]byte(fmt.Sprintf("key-%02d-%03d-%03d", thr, j, k)), nil)
-					assert.NoError(t, err)
-				}
-			}
-		}(i)
-	}
-	wg.Wait()
-	fmt.Printf("Leveldb get batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
-	//Test Get not found performance
-	wg = sync.WaitGroup{}
-	wg.Add(thread)
-	start = time.Now()
-	for i := 0; i < thread; i++ {
-		go func(thr int) {
-			defer wg.Done()
-			for j := 0; j < loop; j++ {
-				for k := 0; k < batchSize; k++ {
-					v, _ := db.Get([]byte(fmt.Sprintf("key-%02d-%03d-x", thr, j)), nil)
-					assert.Nil(t, v)
-				}
-			}
-		}(i)
-	}
-	wg.Wait()
-	fmt.Printf("Leveldb get not found batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
-	//Test delete performance
-	wg = sync.WaitGroup{}
-	wg.Add(thread)
-	start = time.Now()
-	for i := 0; i < thread; i++ {
-		go func(thr int) {
-			defer wg.Done()
-			for j := 0; j < loop; j++ {
-				for k := 0; k < batchSize; k++ {
-					if k%2 == 1 { //delete half of the data
-						err := db.Delete([]byte(fmt.Sprintf("key-%02d-%03d-%03d", thr, j, k)), nil)
-						assert.NoError(t, err)
-					}
-				}
-			}
-		}(i)
-	}
-	wg.Wait()
-	//Test Iterator performance
-	wg = sync.WaitGroup{}
-	wg.Add(thread)
-	start = time.Now()
-	for i := 0; i < thread; i++ {
-		go func(thr int) {
-			defer wg.Done()
-			for j := 0; j < loop; j++ {
-				r := util.BytesPrefix([]byte(fmt.Sprintf("key-%02d-%03d-", thr, j)))
-				iter := db.NewIterator(r, nil)
-				count := 0
-				for iter.Next() {
-					count++
-				}
-				iter.Release()
-				assert.Equal(t, batchSize/2, count)
-			}
-		}(i)
-	}
-	wg.Wait()
-	fmt.Printf("Leveldb iterator batch[%d] thread[%d] loop[%d] cost:%v\n", batchSize, thread, loop, time.Now().Sub(start))
-	db.Close()
-	//Print  folder size
-	size, _ := folderSize(dir)
-	fmt.Printf("Folder[%s] size:%d\n", dir, size)
-}
+
 func folderSize(path string) (int64, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info fs.FileInfo, err error) error {
